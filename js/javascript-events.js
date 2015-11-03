36513dc5497251f56c87184130bcd91f359478cf
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-11-03 13:24:37 -0700

    Check box works. Woo Hoo!

--- js/javascript-events.js
+++ js/javascript-events.js
@@ -1,4 +1,23 @@
+/**
+ * changes the text on a click event
+ **/
+function clickEvent() {
+	document.getElementById("click").innerHTML = "Thank you.";
+}
 
+/**
+ * changes the background color when moused over
+ **/
+function mouseOver() {
+	document.getElementById("mouse").style.backgroundColor = "red";
+}
+
+/**
+ * changes background color when moused out
+ **/
+function mouseOut() {
+	document.getElementById("mouse").style.backgroundColor = "blue";
+}
 
 /**
  * hides/shows the textarea when the checkbox is unchecked/checked
@@ -24,4 +43,14 @@ function popUp() {
  **/
 function closeSignUp() {
 	document.getElementById("signUp").style.display = "none";
+}
+
+/**
+ * generates a kitty based on the dimensions entered =^.^=
+ **/
+function resizeKitty() {
+	var dimension = document.getElementById("dimension").value;
+	var imageUrl  = "http://placekitten.com/g/" + dimension + "/" + dimension;
+	document.getElementById("dimensionValue").innerHTML = dimension;
+	document.getElementById("kitten").src = imageUrl;
 }
